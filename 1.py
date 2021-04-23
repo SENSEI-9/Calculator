2221dfc9e6fbbf7dc86d8da723ef3c94836b17b7
SENSEI-9 <78782813+SENSEI-9@users.noreply.github.com>
2021-04-23 12:01:21 +0545

    Initial commit

---
+++ 1.py
@@ -0,0 +1,11 @@
+from turtle import *
+screen=Screen()
+pen=Turtle()
+pen.speed(0)
+pen.color('red')
+pen.fillcolor('yellow')
+for i in range(500):
+    pen.forward(400)
+    pen.right(100)
+
+done()
